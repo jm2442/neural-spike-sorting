9a760ad331b505067d2f761266edc118855df9f1
James Meyer <40432398+jm2442@users.noreply.github.com>
2020-12-14 13:20:47 +0000

    Ability to plot out training spikes

--- starting-out/messing.py
+++ starting-out/messing.py
@@ -1,6 +1,18 @@
 import scipy.io as spio
+import matplotlib.pyplot as plt 
 
-mat = spio.loadmat('neural-spike-sorting/datasets/training.mat', squeeze_me=True)
+mat = spio.loadmat('../neural-spike-sorting/datasets/training.mat', squeeze_me=True)
 d = mat['d']
 Index = mat['Index']
 Class = mat['Class']
+print('HelloWorld')
+fig, ax1 = plt.subplots()
+color = 'tab:red'
+ax1.set_xlabel("")
+ax1.set_ylabel("Amplitude (mV)", color=color)
+ax1.plot(d, color)
+ax1.tick_params(axis='y', labelcolor=color)
+
+# Show the figure
+fig.tight_layout()
+plt.show()
